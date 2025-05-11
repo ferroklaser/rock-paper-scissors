0961e6250ea5c8520a1246d2e166f560ae1fea46
Win Paing Oo <winpaing000@gmail.com>
2025-05-11 23:21:38 +0800

    Add reset button

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ function playGame() {
     let humanScore = 0;
     let computerScore = 0;
 
+    const display = document.querySelector(".display");
+    const newHumanScore = document.querySelector(".humanScore");
+    const newComputerScore = document.querySelector(".computerScore");
+
     const rock = document.querySelector(".rock");
     rock.addEventListener("click", () => playRound("rock", getComputerChoice()));
 
@@ -13,6 +17,20 @@ function playGame() {
     const scissors = document.querySelector(".scissors");
     scissors.addEventListener("click", () => playRound("scissors", getComputerChoice()));
 
+    const reset = document.querySelector(".reset");
+    reset.addEventListener("click", () => {
+        humanScore = 0;
+        computerScore = 0;
+
+        display.textContent = "";
+        newHumanScore.textContent = humanScore;
+        newComputerScore.textContent = computerScore;
+
+        rock.disabled = false;
+        paper.disabled = false;
+        scissors.disabled = false;
+    })
+
     // const humanChoice = getHumanChoice();
     // playRound(humanChoice, computerChoice);
     
@@ -21,9 +39,6 @@ function playGame() {
         // convert everything to either lower case or uppercase
         humanChoice = humanChoice.toLowerCase();
         computerChoice = computerChoice.toLowerCase();
-        const newHumanScore = document.querySelector(".humanScore");
-        const newComputerScore = document.querySelector(".computerScore");
-        const display = document.querySelector(".display")
     
         if (humanChoice == "scissors") {
             if (computerChoice == "scissors") {
